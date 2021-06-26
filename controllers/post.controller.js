90f3a5ff30f9c56987c71a2045a34a127dabf1c9
Iman Alfathan <alfathan.iman@gmail.com>
2021-06-26 16:59:22 +0700

    master. adding method for creating controller.

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -3,7 +3,7 @@ const Post = db.post;
 const Comment = db.comment;
 const { Op, sequelize, QueryTypes } = require("sequelize");
 
-exports.createPost = (tutorial) => {
+exports.createPost = () => {
   // Validate request
   if (!req.body.title && !req.body.id_user) {
     res.status(400).send({
@@ -56,18 +56,18 @@ exports.findAll = (req, res) => {
     });
 };
 
-// Find a single Tutorial with an id
+// Find a single POST with an id
 exports.findPostById = async (req, res) => {
   const id = req.params.id;
-  const getCommentById = function() {
-  	 var comments =  db.sequelize.query("SELECT * FROM comments where comments.id_post= :id_post", {
-	     replacements: { id_post: id },	
-	     type: db.sequelize.QueryTypes.SELECT, 
-	 	 model: Comment,
- 	 }).then (data => {
-	  	return JSON.stringify(data)
-	 })
-  }
+  // const getCommentById = function() {
+  // 	 var comments =  db.sequelize.query("SELECT * FROM comments where comments.id_post= :id_post", {
+	 //     replacements: { id_post: id },	
+	 //     type: db.sequelize.QueryTypes.SELECT, 
+	 // 	 model: Comment,
+ 	//  }).then (data => {
+	 //  	return JSON.stringify(data)
+	 // })
+  // }
   Post.findByPk(id)
     .then(dataPost => {
      db.sequelize.query("SELECT * FROM comments where comments.id_post= :id_post", {
@@ -93,6 +93,36 @@ exports.findPostById = async (req, res) => {
       res.status(500).send({
       	status:500,
         message: "Error retrieving post with id=" + id
+      });
+    });
+};
+
+exports.createComment = () => {
+  // Validate request
+  if (!req.body.body) {
+    res.status(400).send({
+      message: "Don't give an empty comment!"
+    });
+    return;
+  }
+
+  const comment = {
+    id_post: req.body.id_posts,
+    body: req.body.body,
+  };
+
+  // Save Comment in the database
+  Comment.create(comment)
+    .then(data => {
+      res.status(200).send({
+	      status:200,
+	      message:"Comment successfully added to DB"
+    	});
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the comment."
       });
     });
 };
